3e7b02cbaaddb359ca9fd7601b1cc0f925a162ca
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-01-08 14:10:07 +0530

    operators

---
+++ JavaScript/ArithmeticOperators.js
@@ -0,0 +1,18 @@
+//Arithmatic Operators
+
+/*let a= 30;
+let b= 20;
+
+console.log("a+b =",a+b);*/
+
+let a= 10;
+let b= 20;
+
+console.log("a =",a & "b =",b);
+console.log("a+b = ",a+b);
+console.log("a-b = ",a-b);
+console.log("a*b = ",a*b);
+console.log("a/b = ",a/b);
+console.log("a%b = ",a%b);
+console.log("a**b = ",a**b);   //Exponentiation
+
