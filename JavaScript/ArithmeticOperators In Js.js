43435a832d51f18b064f452ece42e68ba7f19b74
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-01-10 16:00:02 +0530

    Save local changes before pulling remote changes

--- JavaScript/ArithmeticOperators In Js.js
+++ JavaScript/ArithmeticOperators In Js.js
@@ -5,10 +5,10 @@ let b= 20;
 
 console.log("a+b =",a+b);*/
 
-let a= 10;
-let b= 20;
+let a= 5;
+let b= 2;
 
-console.log("a =",a & "b =",b);
+console.log("a = ", a, "& b = ",b);
 console.log("a+b = ",a+b);
 console.log("a-b = ",a-b);
 console.log("a*b = ",a*b);
