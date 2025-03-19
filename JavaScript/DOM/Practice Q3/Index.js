7b85eb93631b0999ba6fa71b01febb006837e4b5
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-03-19 22:12:38 +0530

    Program

---
+++ JavaScript/DOM/Practice Q3/Index.js
@@ -0,0 +1,6 @@
+let newbtn = document.createElement("button");
+newbtn.innerText = "Click Me!!!";
+newbtn.style.color = "white";
+newbtn.style.backgroundColor = "red";
+
+document.querySelector("body").prepend(newbtn);
