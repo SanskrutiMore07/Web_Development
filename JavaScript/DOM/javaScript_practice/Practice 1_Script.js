325b3b8a9fb130fca5eae4ddd36ba51ae2f0c9b2
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-05-05 17:27:25 +0530

    js

---
+++ JavaScript/DOM/javaScript_practice/Practice 1_Script.js
@@ -0,0 +1,19 @@
+//let div = document.querySelector("div");
+//console.log(div);
+
+let id = div.getAttribute("id");
+console.log(id);
+
+let name = div.getAttribute("name");
+console.log(name);
+/// this above methods are used to get attribute value
+
+let para = document.querySelector("p");
+console.log(para.setAttribute("class","newClss"));
+
+// this above methods are used to Set attribute value
+
+let div =document.querySelector("div");
+div.style.backgroundColor ="green";
+div.style.fontSize ="26px";
+div.innerText = "hello!!!!";
