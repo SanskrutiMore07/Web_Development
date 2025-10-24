9165281420a88da9f3bce30385976783f8a0bcd4
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-10-24 21:29:17 +0530

    program

---
+++ javaScript_ForkInfosystems/Object/nonprimitive.js
@@ -0,0 +1,13 @@
+let person ={
+Name : "Shubham",
+Age : 22,
+City :"Kolhapur",
+isStudent : true,
+};
+console.log(person);
+console.log(typeof(person));
+console.log(typeof(person.Name));
+
+let Name = person.Name;
+console.log(Name);
+
