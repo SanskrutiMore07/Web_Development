b46c6cf2ac62afae44a962b698a4762385a699e6
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-12-20 21:38:28 +0530

    program

---
+++ javaScript_ForkInfosystems/Object/ArithmaticOprators.js
@@ -0,0 +1,23 @@
+let a  = 20;
+let b  = 10;
+
+let Result = a+b;
+console.log(Result);
+
+ Result = a-b;
+console.log(Result);
+
+Result = a*b;
+console.log(Result);
+
+Result = a/b;
+console.log(Result);
+
+Result = a&b;
+console.log(Result);
+
+ Result = a**b;   
+console.log(Result);
+
+Result = a%b;   
+console.log(Result);
